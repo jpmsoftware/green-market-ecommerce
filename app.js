0db5e7ba3090120da3ef751efa9d912be24436db
Juan <juanpmiranda92@gmail.com>
2022-07-07 17:55:42 -0300

    heroku commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var routes = require('./routes/routes.js');
+var path = require('path');
 var PORT = process.env.PORT || 5000;
 
 app.use(express.static('./public'));
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -3,24 +3,24 @@ var Product = require('../models/Product');
 
 exports.renderHomePage = async (req, res) => {
   let featuredProducts = await Product.getFeaturedProducts();
-  res.render('/pages/index', {featuredProducts});
+  res.render('./pages/index', {featuredProducts});
 }
 
 exports.renderCartPage = (req, res) => {
-  res.render('/pages/cart');
+  res.render('./pages/cart');
 }
 
 exports.renderSignUpPage = (req, res) => {
-  res.render('/pages/signup');
+  res.render('./pages/signup');
 }
 
 exports.browseCategory = async (req, res) => {
   let products = await Product.getProductsByCategory(req.params.cat);
-  res.render('/pages/category', {products});
+  res.render('./pages/category', {products});
 }
 
 exports.searchProducts = async (req, res) => {
   let query = req.query.search;
   let products = await Product.searchProducts(query);
-  res.render('pages/search', {products});
+  res.render('./pages/search', {products});
 }
