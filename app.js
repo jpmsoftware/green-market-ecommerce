d136f4220857cc50d644bbe80407098bbda654cd
Juan <juanpmiranda92@gmail.com>
2022-07-07 03:53:37 -0300

    package.json fix

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/', routes);
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
